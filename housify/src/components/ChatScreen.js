fdae280c86856103ecf43f1c5cf4a026624a2696
pierretranva <pierretranva@gmail.com>
2023-09-16 21:53:43 -0400

    images show up on message chat

--- housify/src/components/ChatScreen.js
+++ housify/src/components/ChatScreen.js
@@ -1,120 +1,119 @@
-import React, { useState, useEffect } from 'react';
-import { pink } from '@mui/material/colors';
-import { useParams } from 'react-router-dom';
-import './ChatScreen.css';
+import React, { useState, useEffect } from "react";
+import { pink } from "@mui/material/colors";
+import { useParams } from "react-router-dom";
+import "./ChatScreen.css";
 
 function ChatScreen() {
-  const { chatId } = useParams();
-  const [input, setInput] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [messageSender, setMessageSender] = useState('You');
+	const { chatId } = useParams();
+	const [input, setInput] = useState("");
+	const [messages, setMessages] = useState([]);
+	const [messageSender, setMessageSender] = useState("You");
 
-  const handleSend = (e) => {
-    e.preventDefault();
+    useEffect(() =>{
+     console.log(messages)   
+    },[messages])
+	const handleSend = (e) => {
+		e.preventDefault();
 
-    // Create a new message object based on user input
-    const newMessage = { sender: 'You', message: input, image: null };
+		// Create a new message object based on user input
+		const newMessage = { sender: "You", message: input, image: null };
 
-    // Check for specific prompts and provide responses based on chat ID
-    let response = null;
+		// Check for specific prompts and provide responses based on chat ID
+		let response = null;
 
-    if (input.toLowerCase() === 'send pics?') {
-      if (chatId === '1') {
-        response = {
-          sender: 'ChatBot',
-          message: 'Sure!',
-          image: require('../images/Interior/02.a.png'),
-        };
-      } else if (chatId === '2') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      } else if (chatId === '3') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      } else if (chatId === '4') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      } else if (chatId === '5') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      }
-    }
-    if (input.toLowerCase() === 'eco score?') {
-      if (chatId === '1') {
-        response = { sender: 'ChatBot', message: 'No!' };
-      } else if (chatId === '2') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      } else if (chatId === '3') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      } else if (chatId === '4') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      } else if (chatId === '5') {
-        response = { sender: 'ChatBot', message: 'Yes OFC!' };
-      }
-    }
+		if (input.toLowerCase() === "send pics?") {
+			if (chatId === "1") {
+				response = {
+					sender: "ChatBot",
+					message: "Sure!",
+					image: require("../images/Interior/02.a.png"),
+				};
+			} else if (chatId === "2") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			} else if (chatId === "3") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			} else if (chatId === "4") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			} else if (chatId === "5") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			}
+		}
+		if (input.toLowerCase() === "eco score?") {
+			if (chatId === "1") {
+				response = { sender: "ChatBot", message: "No!" };
+			} else if (chatId === "2") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			} else if (chatId === "3") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			} else if (chatId === "4") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			} else if (chatId === "5") {
+				response = { sender: "ChatBot", message: "Yes OFC!" };
+			}
+		}
 
-    // Add the new message to the chat's messages
-    const updatedMessages = [...messages, newMessage];
+		// Add the new message to the chat's messages
+		const updatedMessages = [...messages, newMessage];
 
-    // If there's a response, add it to the messages immediately
-    if (response) {
-      updatedMessages.push(response);
-    }
+		// If there's a response, add it to the messages immediately
+		if (response) {
+			updatedMessages.push(response);
+		}
 
-    // Clear the input after handling
-    setInput('');
+		// Clear the input after handling
+		setInput("");
 
-    // Update the messages state with the new input
-    setMessages(updatedMessages);
-  };
+		// Update the messages state with the new input
+		setMessages(updatedMessages);
+	};
 
-  return (
-    <div className="chatScreen">
-      <p className="chatScreen__timestamp">YOU MATCHED ON 07/11/2020</p>
-      <p>Chat ID: {chatId}</p>
-      <div className="chatScreen__messages">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`chatScreen__message ${
-              message.sender === 'You' ? 'chatScreen__messageUser' : ''
-            }`}
-          >
-            {message.sender !== 'You' && (
-              <div className="chatScreen__avatar" />
-            )}
-            {message.image ? (
-              <img
-                src={message.image.default} // Use .default to access the image URL
-                alt="Sent Image"
-                className="chatScreen__image"
-              />
-            ) : (
-              <p
-                className={`chatScreen__paragraph ${
-                  message.sender === 'You' ? 'chatScreen__paragraphUser' : ''
-                }`}
-              >
-                {message.message}
-              </p>
-            )}
-          </div>
-        ))}
-      </div>
-      <form className="chatScreen__input">
-        <input
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          className="chatScreen__inputField"
-          placeholder="Type a Message...."
-          type="text"
-        />
-        <button
-          onClick={handleSend}
-          variant="contained"
-          style={{ backgroundColor: pink }}
-        >
-          SEND
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="chatScreen">
+			<p className="chatScreen__timestamp">YOU MATCHED ON 07/11/2020</p>
+			<p>Chat ID: {chatId}</p>
+			<div className="chatScreen__messages">
+				{messages.map((message, index) => (
+					<div
+						key={index}
+						className={`chatScreen__message ${message.sender === "You" ? "chatScreen__messageUser" : ""}`}
+					>
+						{message.sender !== "You" && <div className="chatScreen__avatar" />}
+						{message.image ? (
+                            <div>
+							<img
+								src={message.image} // Use .default to access the image URL
+								alt="Sent Image"
+								className="chatScreen__image"
+                                width={400}
+                                height={400}
+							/>
+                            <p className={`chatScreen__paragraph ${message.sender === "You" ? "chatScreen__paragraphUser" : ""}`}>
+                            {message.message}
+                        </p>
+                        </div>
+                            
+						) : (
+							<p className={`chatScreen__paragraph ${message.sender === "You" ? "chatScreen__paragraphUser" : ""}`}>
+								{message.message}
+							</p>
+						)}
+					</div>
+				))}
+			</div>
+			<form className="chatScreen__input">
+				<input
+					value={input}
+					onChange={(e) => setInput(e.target.value)}
+					className="chatScreen__inputField"
+					placeholder="Type a Message...."
+					type="text"
+				/>
+				<button onClick={handleSend} variant="contained" style={{ backgroundColor: pink }}>
+					SEND
+				</button>
+			</form>
+		</div>
+	);
 }
 
 export default ChatScreen;
