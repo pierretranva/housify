997a473d99000a0d8dfa7d2259d38950603c1cf8
Jinjub <124470818+Jinjub@users.noreply.github.com>
2023-09-16 23:24:23 -0400

    added more matches

--- housify/src/data.js
+++ housify/src/data.js
@@ -3,19 +3,21 @@ module.exports = [
         id: 1,
         name: '305 Sinking Spring Holw, New Castle, VA 24127',
         location:'Virginia',
+		year: '1960',
         status: 'For Sale',
         price: '$159,900',
         match: '30',
         description:
             '2 Bed | 2 Bath | 1,434 Square feet',
         message:
-            'Single-family home. Built in 1960. Propane oil for heating. Has 0.4 Acres of land.',
+            'Single-family home. Propane oil for heating. Has 0.4 Acres of land.',
         image: require('./images/Exterior/01.png')
     },
     {
         id: 2,
         name: '1315 Henry Ln, APT D Blackburg, VA 24060',
         location:'Virginia',
+		year: 'n/a',
         status: 'For Rent',
         price: '$1,600/mo',
         match: '93',
@@ -28,6 +30,7 @@ module.exports = [
         id: 3,
         name: '2027 Daylily St, Blacksburg, VA 24060',
         location:'Virginia',
+		year: '2023',
         status: 'Under Contract',
         price: '$390,750',
         match: '94',
@@ -41,18 +44,20 @@ module.exports = [
         id: 4,
         name: '3530 Belaire Dr, Blacksburg, VA 24060',
         location:'Virginia',
+		year: '1974',
         status: 'For Sale',
         price: '$259,900',
         match: '96',
         description:
             '3 Bed | 2 Bath | 1,452 Square feet',
-        message: "Single-family home. Built in 1974. Heat pump for heating and cooling. 1 Acre of land.",
+        message: "Single-family home. Heat pump for heating and cooling. 1 Acre of land.",
         image: require('./images/Exterior/04.png')
     },
     {
         id: 5,
         name: '1402 University City Blvd, #J, Blacksburg, VA 24060',
         location:'Virginia',
+		year: '1987',
         status: 'For Rent',
         price: '$1,300/mo',
         match: '88',
@@ -60,5 +65,70 @@ module.exports = [
             '1 Bed | 1 Bath | 432 Square feet',
         message: "Apartment. Electric, Heat pump for heating. Central air cooling.",
         image: require('./images/Exterior/05.png')
+    },
+	{
+        id: 6,
+        name: '916 Kentwood Dr, Blacksburg, VA 24060',
+        location:'Virginia',
+		year: '2019',
+        status: 'Under Contract',
+        price: '$650,000',
+        match: '79',
+        description:
+            '4 Bed | 4 Bath | 2,246 Square feet',
+        message: "Single-family home. Bike able neighborhood. Electric, heat pump for cooling.",
+        image: require('./images/Exterior/06.png')
+    },
+	{
+        id: 7,
+        name: '2028 Coneflower Ln, Blacksburg, VA 24060',
+        location:'Virginia',
+		year: '2023',
+        status: 'New Construction',
+        price: '$454,900',
+        match: '79',
+        description:
+            '3 Bed | 3 Bath | 2,118 Square feet',
+        message: "Townhome. Not car dependent. 2 car garage.",
+        image: require('./images/Exterior/07.png')
+    },
+	{
+        id: 8,
+        name: '922 Kabrich St, Blacksburg, VA 24060',
+        location:'Virginia',
+		year: '2008',
+        status: 'For Sale',
+        price: '$720,000',
+        match: '91',
+        description:
+            '6 Bed | 6 Bath | 3,306 Square feet',
+        message: "Townhome. Electric for water heater. Heat pump for cooling and heating.",
+        image: require('./images/Exterior/08.png')
+    },
+	{
+        id: 9,
+        name: '1222 Patrick Henry Dr, Blacksburg, VA 24060',
+        location:'Virginia',
+		year: '2019',
+        status: 'Under Contract',
+        price: '$875-$1,100/mo',
+        match: '69',
+        description:
+            '2-4 Bed | 2-4 Bath | 859-1,484 Square feet',
+        message: "Apartment. Pool. 24 hr gym. Furnished unit.",
+        image: require('./images/Exterior/09.png')
+    },
+	{
+        id: 10,
+        name: '1711 Whipple Dr, Blacksburg, VA 24060',
+        location:'Virginia',
+		year: '2019',
+        status: 'Under Comtract',
+        price: '$525/mo',
+        match: '79',
+        description:
+            '4 Bed | 2 Bath | 1,058 Square feet',
+        message: "Apartment. Furnished unit. Separate leases",
+        image: require('./images/Exterior/10.png')
     },
 ];
